522856b94eb4205972f6d0758ec196561abf4e6e
Nikolay Vasilev <gozebone@gmail.com>
2025-09-27 04:49:53 +0300

    - hls and kotlin

--- init.lua
+++ init.lua
@@ -621,8 +621,8 @@ require('lazy').setup({
         stylua = {}, -- Used to format Lua code
         -- Special Lua Config, as recommended by neovim help docs
         --
-        kotlin_language_server = {},
-        hls = {},
+        -- kotlin_language_server = {},
+        -- hls = {},
         lua_ls = {
           on_init = function(client)
             client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
